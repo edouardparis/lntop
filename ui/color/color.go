0e24b6f55aa3de3a21071ab67afbb06a88c49c19
Edouard Paris <m@edouard.paris>
2019-03-22 14:42:28 +0100

    ft new colors

--- ui/color/color.go
+++ ui/color/color.go
@@ -8,6 +8,7 @@ var (
 	Green   = color.New(color.FgGreen).SprintFunc()
 	GreenBg = color.New(color.BgGreen, color.FgBlack).SprintFunc()
 	Red     = color.New(color.FgRed).SprintFunc()
+	RedBg   = color.New(color.BgRed, color.FgBlack).SprintFunc()
 	Cyan    = color.New(color.FgCyan).SprintFunc()
 	CyanBg  = color.New(color.BgCyan, color.FgBlack).SprintFunc()
 	WhiteBg = color.New(color.BgWhite, color.FgBlack).SprintFunc()
