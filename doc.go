f869a471b5e25b739ed927ad3ab71b15ce765c21
Edouard Paris <m@edouard.paris>
2019-04-11 13:18:11 +0200

    add doc.go

---
+++ doc.go
@@ -0,0 +1,11 @@
+// lntop v0.0.0
+// Released under the MIT License
+//
+// Lightning is a decentralized network using smart contract functionality
+// in the Bitcoin protocol to enable instant payments across a network of
+// participants. Precisely bidirectional payments channels are set up
+// between participants. For more information: http://lightning.network.
+//
+// lntop is an interactive text-mode channels viewer for Unix systems.
+// It supports for the moment the Go implementation lnd only.
+package lntop
--- ui/views/help.go
+++ ui/views/help.go
@@ -50,7 +50,7 @@ func (h Help) Set(g *gocui.Gui, x0, y0, x1, y1 int) error {
 		}
 	}
 	h.view.Frame = false
-	fmt.Fprintln(h.view, "lntop 0.0.1 - (C) 2019 Edouard Paris")
+	fmt.Fprintln(h.view, "lntop 0.0.0 - (C) 2019 Edouard Paris")
 	fmt.Fprintln(h.view, "Released under the MIT License")
 	fmt.Fprintln(h.view, "")
 	fmt.Fprintln(h.view, fmt.Sprintf("%5s %s",
